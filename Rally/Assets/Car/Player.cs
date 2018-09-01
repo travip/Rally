50acb43c117eb6011a60e06320bf3ab4721397b2
Travis <tgpovey@gmail.com>
2018-09-01 11:26:47 +0800

    Player actions

---
+++ Rally/Assets/Car/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public enum ActionType { RightBend, RightTurn, RightSquare, RightAcute, LeftBend, LeftTurn, LeftSquare, LeftAcute, Fast, Slow }
+    private readonly Queue<ActionType> PlayerActions = new Queue<ActionType>();
+
+    public int MaxInputs { get; private set; }
+
+    public ActionType[] GetPlayerActions()
+    {
+        return PlayerActions.ToArray();
+    }
+
+    public bool PlayerInput(ActionType action)
+    {
+        if (PlayerActions.Count < MaxInputs)
+        {
+            PlayerActions.Enqueue(action);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public ActionType GetNextAction()
+    {
+        return PlayerActions.Dequeue();
+    }                
+}
