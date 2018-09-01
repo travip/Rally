b3f3e85fc226dc034a69c917b06ba6720e04f8f1
James White <jamesgeewhite@gmail.com>
2018-09-01 11:49:40 +0100

    Add editor script to align all roads

--- Rally/Assets/Editor/debugmenu.cs
+++ Rally/Assets/Editor/debugmenu.cs
@@ -3,28 +3,40 @@ using UnityEngine;
 using UnityEditor;
 
 public static class DebugMenu {
-	// [MenuItem("Debug/Print Global Position")]
+	
 
 	private static Vector3 position;
 	private static Quaternion rotation;
 
 	[MenuItem("CONTEXT/Transform/Copy Global Transform", false, 151)]
-	public static void CopyGlobalPosition () {
-		//if (Selection.activeGameObject != null) {
-		//	Debug.Log(Selection.activeGameObject.name + " is at " + Selection.activeGameObject.transform.position);
-		//}
-
+	public static void CopyGlobalPosition() {
 		position = Selection.activeTransform.position;
 		rotation = Selection.activeTransform.rotation;
 	}
 
 	[MenuItem("CONTEXT/Transform/Paste Global Transform", false, 201)]
-	public static void PasteGlobalPosition () {
+	public static void PasteGlobalPosition() {
 		Transform[] selections = Selection.transforms;
 		foreach (Transform selection in selections) {
 			Undo.RecordObject(selection, "Paste Global Transform" + selection.name);
 			selection.position = position;
 			selection.localRotation = rotation;
+		}
+	}
+
+	[MenuItem("Debug/Align roads")]
+	public static void AlignRoads() {
+		Debug.Log("Aligning all roads in the \"Roads\" gameobject");
+		Transform roads = GameObject.Find("Roads").transform;
+		Vector3 pos = Vector3.zero;
+		Quaternion rot = Quaternion.identity;
+
+		foreach (Transform road in roads) {
+			road.position = pos;
+			road.rotation = rot;
+			Transform end = road.Find("end");
+			pos = end.position;
+			rot = end.rotation;
 		}
 	}
 }
