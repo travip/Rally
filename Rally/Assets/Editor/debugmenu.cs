d0c4b495c968a93ea90ec2d7883b2def17792da9
James White <jamesgeewhite@gmail.com>
2018-09-01 11:18:47 +0100

    Editor script to copy-paste global transform

---
+++ Rally/Assets/Editor/debugmenu.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public static class DebugMenu {
+	// [MenuItem("Debug/Print Global Position")]
+
+	private static Vector3 position;
+	private static Quaternion rotation;
+
+	[MenuItem("CONTEXT/Transform/Copy Global Transform", false, 151)]
+	public static void CopyGlobalPosition () {
+		//if (Selection.activeGameObject != null) {
+		//	Debug.Log(Selection.activeGameObject.name + " is at " + Selection.activeGameObject.transform.position);
+		//}
+
+		position = Selection.activeTransform.position;
+		rotation = Selection.activeTransform.rotation;
+	}
+
+	[MenuItem("CONTEXT/Transform/Paste Global Transform", false, 201)]
+	public static void PasteGlobalPosition () {
+		Transform[] selections = Selection.transforms;
+		foreach (Transform selection in selections) {
+			Undo.RecordObject(selection, "Paste Global Transform" + selection.name);
+			selection.position = position;
+			selection.localRotation = rotation;
+		}
+	}
+}
+#endif
